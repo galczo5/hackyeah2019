c6a0f4fd5edae3f1a7de7757e0254008183f5cf9
Mike <michal.stasiak.92@gmail.com>
2019-09-15 03:01:06 +0200

    Avatars

--- server/src/main/java/pl/terminal/server/infrastructure/traveler/avatar/JpaAvatar.java
+++ server/src/main/java/pl/terminal/server/infrastructure/traveler/avatar/JpaAvatar.java
@@ -12,9 +12,10 @@ import javax.persistence.*;
 @AllArgsConstructor
 public class JpaAvatar {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(EnumType.STRING)
     private Nationality nationality;
 
     @Lob
